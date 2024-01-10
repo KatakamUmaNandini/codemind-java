e8ba732df5b204e8922998dc608a86425e22fca1
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-01-10 19:11:39 +0530

    import java.util.Scanner; public class Program{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         double ans=(a*5)/18;         System.out.printf("%.2f",ans);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        double ans=(a*5)/18;
+        System.out.printf("%.2f",ans);
+    }
+}
