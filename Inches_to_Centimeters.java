731950fa89944a6ff633badcbfd45408e6157a08
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-02-04 18:39:13 +0530

    import java.util.Scanner; public class Jaca{     public static void main(String[] ardd){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double k = n*2.54;         System.out.printf("%.2f",k);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Jaca{
+    public static void main(String[] ardd){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double k = n*2.54;
+        System.out.printf("%.2f",k);
+    }
+}
