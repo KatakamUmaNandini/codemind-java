28c92d84b9058b97c873a650ca30b2087791827e
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-02-04 20:13:42 +0530

    import java.util.Scanner; public class Java{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         char s = sc.next().charAt(0);         int a = (int)s;         System.out.print(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char s = sc.next().charAt(0);
+        int a = (int)s;
+        System.out.print(a);
+    }
+}
