5e4b1ca82dd61f17f0d9045f9fef4169fa5c4df6
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-02-14 10:57:38 +0530

    import java.util.Scanner; public class Java{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         System.out.print(s.charAt(7-1));     } }

---
+++ Lucky_Character.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.print(s.charAt(7-1));
+    }
+}
