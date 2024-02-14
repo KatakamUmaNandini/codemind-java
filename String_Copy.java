a99c075906dd6d6849ec35e8532e48f8bfa78cc3
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-02-14 11:43:24 +0530

    import java.util.Scanner; public class Java{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         String s1 = s;         System.out.print(s1);     } }

---
+++ String_Copy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String s1 = s;
+        System.out.print(s1);
+    }
+}
