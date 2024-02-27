93580e4dc5cce464dd290e532409a254968a7195
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2024-02-27 16:13:45 +0530

    import java.util.Scanner; public class Java{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         if(x>=18){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Java{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x>=18){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
